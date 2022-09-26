0aabda54eda330238d2a18e27f7ac785f53b7962
tweimann <t@tweimann.xyz>
2022-09-26 22:03:57 +0200

    working api

--- src/app.js
+++ src/app.js
@@ -2,14 +2,24 @@
 const log = require('./log.js')
 const pw = require('./password.js')
 const pp = require('./passphrase.js')
+const { config } = require('dotenv')
+config()
+
+// get dotenv vars
+const settings = {
+    debug: process.env.DEBUG,
+    tele: process.env.TELEMETRY,
+    imprint: process.env.IMPRINTADDR
+}
 
 // hello world
 log.console('info', 'Hello World!')
 
 // debug
-console.log(pw.generate("AasnO", 10))
-
-setTimeout(function() {
-    console.log(pp.generate("DEN", 4, "-"))
-    console.log(pp.generate("DENC", 4, "-"))
-}, 2000)
+if (settings.debug) {
+    setTimeout(function() {
+        console.log(pw.generate("AasnO", 10))
+        console.log(pp.generate("DEN", 4, "-"))
+        console.log(pp.generate("DENC", 4, "-"))
+    }, 2000)
+}
